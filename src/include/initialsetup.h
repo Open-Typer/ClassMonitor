28877b25cefc121cc00d40aa189c906f86c0ba9e
adazem009 <68537469+adazem009@users.noreply.github.com>
2022-01-05 20:32:31 +0100

    initialsetup: Add docs image

--- src/include/initialsetup.h
+++ src/include/initialsetup.h
@@ -2,7 +2,7 @@
  * initialsetup.h
  * This file is part of Open-Typer
  *
- * Copyright (C) 2021 - adazem009
+ * Copyright (C) 2021-2022 - adazem009
  *
  * Open-Typer is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -29,7 +29,10 @@ namespace Ui {
 	class initialSetup;
 }
 
-/*! \brief The initialSetup class is a dialog for initial program setup. */
+/*!
+ * \brief The initialSetup class is a dialog for initial program setup.
+ * \image html initialSetup.png
+ */
 class initialSetup : public QDialog
 {
 	Q_OBJECT
